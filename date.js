24d001cab749586325a95d0acd6ad8fa5b422c29
mm <m.mazharulislam333@gmail.com>
2024-05-25 12:28:36 +0600

    dd-mm-yyyy

---
+++ date.js
@@ -0,0 +1,10 @@
+
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth() + 1; 
+let year = date.getFullYear(); 
+day = day < 10 ? '0' + day :day;
+month = month < 10 ? '0'+ month: month;
+
+let fullDate = day + "-" + month + "-" + year;
+document.getElementById('today').innerHTML = fullDate;
